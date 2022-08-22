386debd53ca7ba84314686594fc3f37627add55b
shachar1236 <52973001+shachar1236@users.noreply.github.com>
2022-08-22 14:56:57 +0300

    Added move between tabs keybinding

--- lua/personal.lua
+++ lua/personal.lua
@@ -1,4 +1,6 @@
-------- personal config -------
+------- personal config ---------
+
+-- move lines with alt --
 vim.api.nvim_set_keymap("i", "ww", "<ESC>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<A-j>", ":m .+1<CR>==", { noremap = true })
 vim.api.nvim_set_keymap("n", "<A-k>", ":m .-2<CR>==", { noremap = true })
@@ -6,3 +8,11 @@ vim.api.nvim_set_keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { noremap = true })
 vim.api.nvim_set_keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { noremap = true })
 vim.api.nvim_set_keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true })
 vim.api.nvim_set_keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true })
+-- move between buffers --
+vim.api.nvim_set_keymap("n", "g<tab>", ":bnext<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "g<s-tab>", ":bprev<CR>", { noremap = true })
+
+-- tab size --
+vim.api.nvim_set_option("tabstop", 4)
+vim.api.nvim_set_option("shiftwidth", 4)
+vim.api.nvim_set_option("expandtab", true)
